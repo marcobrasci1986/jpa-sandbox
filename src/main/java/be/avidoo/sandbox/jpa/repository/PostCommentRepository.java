43458ef271cb56180a9f5a085e0928bdafe95019
marcobrasci1986 <marco_brasci@hotmail.com>
2024-02-02 16:51:30 +0100

    Working version

--- src/main/java/be/avidoo/sandbox/jpa/repository/PostCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package be.avidoo.sandbox.jpa.repository;
-
-import be.avidoo.sandbox.jpa.model.PostComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
-}
--- src/main/resources/db/migration/V2__post_post_comment.sql
+++ src/main/resources/db/migration/V2__post_post_comment.sql
@@ -1,16 +1,16 @@
-CREATE SEQUENCE sandbox.post_seq START 1;
-CREATE SEQUENCE sandbox.post_comment_seq START 1;
+CREATE SEQUENCE post_seq START 1;
+CREATE SEQUENCE post_comment_seq START 1;
 
-CREATE TABLE IF NOT EXISTS sandbox.post
+CREATE TABLE IF NOT EXISTS post
 (
     id    int     NOT NULL PRIMARY KEY,
     title varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS sandbox.post_comment
+CREATE TABLE IF NOT EXISTS post_comment
 (
     id      int     NOT NULL PRIMARY KEY,
     review  varchar NOT NULL,
     post_id int,
-    CONSTRAINT fk_post_comment_post_id FOREIGN KEY (post_id) REFERENCES sandbox.post
+    CONSTRAINT fk_post_comment_post_id FOREIGN KEY (post_id) REFERENCES post
 );
